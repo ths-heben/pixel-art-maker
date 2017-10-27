b97799356b8942841aa9fa51d4c640730f1dfc7a
Thomas Hebenstreit <thomas.hebenstreit@xal.com>
2017-10-27 13:03:30 +0200

    feat: Create custom grid and mark cells
    Create a grid and select a color. Mark cells by clicking on them.

---
+++ designs.js
@@ -0,0 +1,46 @@
+// Select color input
+var color;
+$('#colorPicker').change(function() {
+  color = $(this).val();
+});
+
+// Select size input
+// Height in rows
+var height;
+$('#input_height').change(function() {
+  height = $(this).val();
+});
+
+// Width in columns
+var width;
+$('#input_width').change(function() {
+  width = $(this).val();
+});
+
+
+// When size is submitted by the user, call makeGrid()
+$('form').submit(function(event) {
+  $('.row').remove();
+  event.preventDefault();
+
+  if (width && height && color) {
+    makeGrid();
+  }
+  else {
+    alert('Fail: Add a grid for going on! ;)')
+  }
+});
+
+function makeGrid() {
+  // Your code goes here!
+  for (var i = 0; i < height; i++) {
+    $('table').append('<tr class="row" id="' + i + '"></tr>');
+  }
+  for (var j = 0; j < width; j++) {
+    $('.row').append('<td id=' + j + '></td>');
+  }
+
+  $('td').on('click', function() {
+    $(this).css('background-color', color);
+  });
+}
